240ba4cace99267657144edc9a836bfd36d88548
aman-kumar-crayon <114092660+aman-kumar-crayon@users.noreply.github.com>
2023-11-14 06:39:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,3 +38,40 @@ resource "google_container_cluster" "primary" {
   ip_allocation_policy {
     cluster_ipv4_cidr_block  = "10.11.0.0/21"
     services_ipv4_cidr_block = "10.12.0.0/21"
+ }
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "10.0.0.7/32"
+      display_name = "net1"
+    }
+
+  }
+}
+/*
+# Create managed node pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = "asia-south2-a"
+  cluster    = google_container_cluster.primary.name
+  node_count = 3
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = "dev"
+    }
+
+    machine_type = "n1-standard-1"
+    preemptible  = false
+    #service_account = google_service_account.mysa.email
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+*/
