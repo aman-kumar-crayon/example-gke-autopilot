3d33a074b00b7772295d5ca60fe8cbae1a249968
aman-kumar-crayon <114092660+aman-kumar-crayon@users.noreply.github.com>
2023-11-16 19:17:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "google_container_cluster" "primary" {
   #enable_autopilot = true
   network                  = google_compute_network.vpc.name
   subnetwork               = google_compute_subnetwork.subnet.name
-  remove_default_node_pool = true                ## create the smallest possible default node pool and immediately delete it.
-  # networking_mode          = "VPC_NATIVE" 
+  #remove_default_node_pool = true                ## create the smallest possible default node pool and immediately delete it.
+  networking_mode          = "VPC_NATIVE" 
   initial_node_count       = 1
   
   private_cluster_config {
