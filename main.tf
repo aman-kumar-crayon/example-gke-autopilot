fe78cdbafe9ea707d7169267ab6a96d47450873c
aman-kumar-crayon <114092660+aman-kumar-crayon@users.noreply.github.com>
2023-11-16 16:13:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "google_container_cluster" "primary" {
 master_authorized_networks_config {
 cidr_blocks {
 
-cidr_blocks = "10.0.0.7/32"
+cidr_block = "10.0.0.7/32"
 }
 gcp_public_cidrs_access_enabled = false
 
