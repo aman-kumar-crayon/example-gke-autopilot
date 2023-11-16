f60c0767e9d1c030455dd32c00a3651d87f949b0
aman-kumar-crayon <114092660+aman-kumar-crayon@users.noreply.github.com>
2023-11-16 07:39:02 +0100

    Create install_proxy.sh

---
+++ install_proxy.sh
@@ -0,0 +1,33 @@
+# Install squid proxy
+export DEBIAN_FRONTEND=noninteractive
+apt-get -yy update
+apt-get -yy install squid
+cat << EOF > /etc/squid/squid.conf
+acl localnet src 10.0.0.0/8
+acl localnet src fc00::/7
+acl localnet src fe80::/10
+acl to_metadata dst 169.254.169.254
+
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 443
+acl CONNECT method CONNECT
+
+http_access deny !Safe_ports
+http_access deny CONNECT !SSL_ports
+http_access deny to_metadata
+http_access allow localhost manager
+http_access deny manager
+
+include /etc/squid/conf.d/*
+
+http_access allow localnet
+http_access deny all
+http_port 3128
+visible_hostname proxy.example.internal
+
+access_log none
+EOF
+
+systemctl enable squid.service
+systemctl restart squid.service
