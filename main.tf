abe634679448cebce519737323e98a2a9b223f3c
aman-kumar-crayon <114092660+aman-kumar-crayon@users.noreply.github.com>
2023-11-16 10:21:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,22 @@ resource "google_compute_subnetwork" "subnet" {
 depends_on = [google_compute_network.vpc]
 }
 
+resource "google_compute_router" "router" {
+  name    = "cloud-router"
+  region        = "europe-west3"
+  network = google_compute_network.vpc.id
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "cloud-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+depends_on = [google_compute_router.router]
+}
+
+
 # # Create Service Account
 # resource "google_service_account" "mysa" {
 #   account_id   = "mysa"
